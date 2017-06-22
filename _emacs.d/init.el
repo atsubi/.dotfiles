b9cc91d24b865e178cdae97b6cfb02c946b6679c
atsubi <wisemagic51@gmail.com>
2017-06-23 07:28:36 +0900

    add emacs markdown mode

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,4 +1,11 @@
-;; load-path
+;; Load-path
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path (&rest paths)
   (mapc '(lambda (path)
 	   (add-to-list 'load-path path))
@@ -7,7 +14,6 @@
 ;; lisp directory's path
 (add-to-load-path "~/.emacs.d/auto-complete")
 (add-to-load-path "~/.emacs.d/elpa")
-
 
 
 ;; auto-complete
@@ -29,10 +35,11 @@
 
 
 (custom-set-faces
-'(hl-line ((t (:background "color-236"))))
-)
-
-
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:background "color-236")))))
 
 ; (yes/no) -> (y/n)
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -42,3 +49,24 @@
 
 ;;; .#*いらない
 (setq auto-save-default nil)
+
+
+;;; package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (nlinum))))
+
+
+;;; markdown
+(autoload 'markdown-mode "~/.emacs.d/markdown-mode.el" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;;; markdown preview
+;; browser
+(setq markdown-command "~/.emacs.d/Markdown.pl")
